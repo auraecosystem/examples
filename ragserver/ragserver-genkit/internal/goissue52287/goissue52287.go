cecb48c311e48ba1494f536e6c141f7c10f6b549
Dmitri Shuralyov <dmitshur@golang.org>
2024-09-17 16:22:53 -0400

    ragserver: don't try to build on 32-bit Go ports
    Trying to only build some of these commands on 32-bit ports fails
    due to a build error in a dependency:
    
    	$ GOOS=linux GOARCH=386 go build golang.org/x/example/ragserver/ragserver
    	# github.com/weaviate/weaviate/entities/vectorindex/common
    	$GOMODCACHE/github.com/weaviate/weaviate@v1.26.1/entities/vectorindex/common/config.go:59:14: constant 9223372036854775807 overflows int
    
    That particular error is due to it trying to convert math.MaxInt64
    to an 'int' type.
    
    Working on 32-bit Go ports seems out of scope for these examples.
    Add build constraints that make this limitation explicit.
    
    Runs into golang/go#52287 (unfortunately).
    
    Change-Id: I9db291fbb06cfd9eed1df318aff6a259b7603f95

---
+++ ragserver/ragserver-genkit/internal/goissue52287/goissue52287.go
@@ -0,0 +1,7 @@
+// Copyright 2024 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package goissue52287 is an empty internal package.
+// It exists only to work around go.dev/issue/52287.
+package goissue52287
